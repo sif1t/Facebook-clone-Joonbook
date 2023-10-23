f6171c2517d6ba32058451374e281f2dd866aabc
arifeensifat01 <arifeen.sifat01@gmail.com>
2023-10-23 21:57:45 +0600

    dark thim now working

--- main.js
+++ main.js
@@ -1,5 +1,14 @@
 var settingmenu =document.querySelector('.setting-menu')
+var darkbtn =document.getElementById("dark-btn")
 
 function SettingMenuToggle (){
     settingmenu.classList.toggle("setting-menu-height");
 }
+
+darkbtn.onclick = function(){
+    darkbtn.classList.toggle("dark-btn-on")
+    document.body.classList.toggle("dark-theme")
+}
+
+
+
