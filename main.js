1d34b1b6e39cdbd0bac013b031eb59be9491a1c0
arifeensifat01 <arifeen.sifat01@gmail.com>
2023-10-24 17:27:12 +0600

    add new profile page section

--- main.js
+++ main.js
@@ -6,9 +6,36 @@ function SettingMenuToggle (){
 }
 
 darkbtn.onclick = function(){
-    darkbtn.classList.toggle("dark-btn-on")
-    document.body.classList.toggle("dark-theme" )
+    darkbtn.classList.toggle("dark-btn-on");
+    document.body.classList.toggle("dark-theme" );
+
+    
+    // if(localStorage.getItem("theme") == "light"){
+    //     localStorage.setItem("theme", "dark");
+    // }
+    // else{
+    //     localStorage.getItem("theme","light");
+    // }
+
+
 }
+
+
+
+// if(localStorage.getItem("theme") == "light"){
+//     darkbtn.classList.remove("dark-btn-on");
+//     document.body.classList.remove("dark-theme" );
+// }
+// else if (localStorage.getItem("theme") == "dark"){
+//     darkbtn.classList.add("dark-btn-on");
+//     document.body.classList.add("dark-theme" );
+// }
+
+// else{
+//     localStorage.setItem("theme", "light");
+// }
+
+
 
 
 
